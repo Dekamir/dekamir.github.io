f244c3a8c7ce3ed54c7048470b635e65e79bed74
Yunus İşleyen <dekamir@outlook.com>
2024-07-06 06:35:03 +0300

    Add first popup (Close all windows)

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ import Window from "./Window.js";
 
 const about = new Window(document.querySelector("#window-about_user"));
 const projects = new Window(document.querySelector("#window-projects"));
+const close_all_windows = new Window(document.querySelector("#window-close_all_windows"));
