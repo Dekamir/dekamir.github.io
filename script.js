0660c728c01df02d05a5e7f53b67b122e177fd02
J3ekir <bekir911@hotmail.com>
2024-07-27 00:08:33 +0300

    Add PhotoSwipe

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+import PhotoSwipeLightbox from "./photoswipe/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "./photoswipe/photoswipe.esm.min.js";
 import Window from "./Window.js";
 
 const about = new Window(document.querySelector("#window-about_user"));
 const projects = new Window(document.querySelector("#window-projects"));
 const close_all_windows = new Window(document.querySelector("#window-close_all_windows"));
+
+document.querySelectorAll(".catalogue-item").forEach(elem => {
+    elem.setAttribute("data-pswp-width", elem.firstElementChild.naturalWidth);
+    elem.setAttribute("data-pswp-height", elem.firstElementChild.naturalHeight);
+});
+
+const lightbox = new PhotoSwipeLightbox({
+    gallery: ".catalogueview-catalogue",
+    children: ".catalogue-item",
+    pswpModule: PhotoSwipe,
+});
+
+lightbox.init();
