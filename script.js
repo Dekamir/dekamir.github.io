dbda70e92b918964c5732c6e81e2e57cd6aa0eee
J3ekir <bekir911@hotmail.com>
2024-08-04 18:14:43 +0300

    Add caption to PhotoSwipe slides

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import PhotoSwipeDynamicCaption from "./photoswipe/photoswipe-dynamic-caption-plugin.esm.min.js";
 import PhotoSwipeLightbox from "./photoswipe/photoswipe-lightbox.esm.min.js";
 import PhotoSwipe from "./photoswipe/photoswipe.esm.min.js";
 import Window from "./Window.js";
@@ -15,6 +16,18 @@ const lightbox = new PhotoSwipeLightbox({
     gallery: ".catalogueview-catalogue",
     children: ".catalogue-item",
     pswpModule: PhotoSwipe,
+    paddingFn(viewportSize) {
+        return {
+            top: 30, bottom: 30, left: 0, right: 0
+        };
+    },
+});
+
+const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+    type: "auto",
+    captionContent(slide) {
+        return slide.data.element.nextElementSibling?.matches(".pswp-caption-content") && slide.data.element.nextElementSibling?.innerHTML || slide.data.element.querySelector("img").getAttribute("title");
+    }
 });
 
 lightbox.init();
